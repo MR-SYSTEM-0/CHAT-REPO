1b0a77ef24dfcd88883d7acf1743cd9861cf4f00
diangrey <diangreyling0705@gmail.com>
2025-12-19 13:12:13 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "22243185"
+# -------------------------------------------------------------
+API_HASH = "39d926a67155f59b722db787a23893ac"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = "1999645649"
+SUPPORT_GRP = "INDIAN_CHATTING_GC"
+UPDATE_CHNL = "about_candyy"
+OWNER_USERNAME = "candy_caugh"
