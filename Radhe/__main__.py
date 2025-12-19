ed4a9b479530a2694403a9c8426fddb3dc45d47c
diangrey <diangreyling0705@gmail.com>
2025-12-19 13:05:28 +0530

    Create __main__.py

---
+++ Radhe/__main__.py
@@ -0,0 +1,26 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+
+from Radhe import LOGGER, Radhe
+from Radhe.modules import ALL_MODULES
+
+
+async def anony_boot():
+    try:
+        await Radhe.start()
+    except Exception as ex:
+        LOGGER.error(ex)
+        quit(1)
+
+    for all_module in ALL_MODULES:
+        importlib.import_module("Radhe.modules." + all_module)
+
+    LOGGER.info(f"@{Radhe.username} Started.")
+    await idle()
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(anony_boot())
+    LOGGER.info("Stopping Radhe Bot...")
