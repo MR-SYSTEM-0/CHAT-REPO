339777ab32c715e420576625910a333142dab137
MR-SYSTEM-0 <mrhari659@gmail.com>
2026-01-04 13:35:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "22243185"
+API_ID = ""
 # -------------------------------------------------------------
-API_HASH = "39d926a67155f59b722db787a23893ac"
+API_HASH = ""
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "1999645649"
-SUPPORT_GRP = "INDIAN_CHATTING_GC"
-UPDATE_CHNL = "about_candyy"
-OWNER_USERNAME = "candy_caugh"
+OWNER_ID = "5311223486"
+SUPPORT_GRP = "muzicbot19"
+UPDATE_CHNL = "systemlk"
+OWNER_USERNAME = "System_91"
